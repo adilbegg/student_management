527115751f937129c4cc22939263fba05690b397
adil <mirzaadilbeg9@gmail.com>
2026-07-02 20:42:03 +0530

    Add student management project files

--- student.py
+++ student.py
@@ -20,14 +20,53 @@ class Student:
           return 0
         
     def get_grade(self):
-       
-       for min_mark, grade in self.grade_scale:
-          if self.get_percentage() >= min_mark:
+        percentage = self.get_percentage()
+
+        for min_mark, grade in self.grade_scale:
+          if percentage >= min_mark:
              return grade
+          
+        return 'F'
        
+    def is_passing(self):
+      if self.get_percentage() >= 45:
+         return True
+      else:
+         return False 
+      
+    def __str__(self):
+       return f"[{self.student_id}] {self.name} | Age: {self.age} | Course: {self.course}"
+
+
+    def to_csv_row(self):
+       marks_str = ';'.join(f"{subject}:{mark}" for subject, mark in self.marks.items())
+       return [self.student_id, self.name, self.age, self.course, marks_str]
+       
+
+    @staticmethod
+    def from_csv_row(row):
+       student_id, name, age, course, marks_str = row
+       marks = dict()
+
+       if marks_str:
+          for i in marks_str.split(';'):
+             if i:
+                subject, mark = i.split(':')
+                marks[subject] = int(mark)
+                
+       return Student(student_id=student_id, name=name, age=int(age), course=course, marks=marks)
+
 
 if __name__ == '__main__':
  s = Student('SMS-0001', 'Aisha', 20, 'CS', {'Maths': 44, 'English': 44})
  print(s.student_id, s.name, s.marks)
  print(s.get_percentage())
  print(s.get_grade())
+ print(s.is_passing())
+ print(s)
+
+row = s.to_csv_row()
+print(row)
+
+s2 = Student.from_csv_row(row)
+print(s2.name, s2.marks)
